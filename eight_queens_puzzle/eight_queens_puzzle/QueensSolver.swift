1e0fdc7b0aa47165056e5695687d2af103ca70d7
aaron chen <ixdezbtoeck@gmail.com>
2020-02-18 19:53:01 -0800

    finish eight queens puzzle

--- eight_queens_puzzle/eight_queens_puzzle/QueensSolver.swift
+++ eight_queens_puzzle/eight_queens_puzzle/QueensSolver.swift
@@ -15,9 +15,30 @@ import Foundation
 /// - You are allowed to change the function header (args or return type)
 /// - Your total recursive calls should not exceed 120 times.
 
-//var count = 0
+
+var count = 0
+
 func solveQueens(board: inout Board) {
-//	count += 1
-	
+    helpingFunction(i: 0, puted: 0, board: &board)
 }
+
+func helpingFunction(i:Int,puted:Int = 0,board: inout Board){
+    if(puted == 8){
+        count += 1
+        print(board)
+        print(count)
+    }
+    else{
+        for bi in i..<board.size*board.size{
+            let ci = bi % board.size
+            let ri = (bi-ci)/board.size
+            if(board.isSafe(row: ri, col: ci)){
+                board.place(row: ri, col: ci)
+                helpingFunction(i: bi, puted: puted+1, board: &board)
+                board.remove(row: ri, col: ci)
+            }
+        }
+    }
+}
+
 
--- eight_queens_puzzle/eight_queens_puzzle/main.swift
+++ eight_queens_puzzle/eight_queens_puzzle/main.swift
@@ -2,11 +2,11 @@
 //  main.swift
 //  eight_queens_puzzle
 //
-//  Created by Aaron Chen on 2020-02-14.
+//  Created by Aaron Chen on 2020-02-18.
 //  Copyright © 2020 Aaron Chen. All rights reserved.
 //
 
 import Foundation
 
-print("Hello, World!")
-
+var board = Board(size: 8)
+solveQueens(board: &board)
