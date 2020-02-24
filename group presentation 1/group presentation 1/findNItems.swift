4ad494b90ab58cfa3d6f145cefdf90adf36a4373
aaron chen <ixdezbtoeck@gmail.com>
2020-02-24 00:06:10 -0800

    use heap keep priority queue

--- group presentation 1/group presentation 1/findNItems.swift
+++ group presentation 1/group presentation 1/findNItems.swift
@@ -39,40 +39,37 @@ func stdin(){
 }
 
 func findNItems<T:Comparable>(_ items:[T],_ m: Int,_ comparator:(T,T)->Bool) ->[T]{
-    var stream:[T] = [T]()
+    var heap:[T] = [T]()
+    
     for i in items{
-        /*if stream is empty add item anyway**/
-        if(stream.count==0){
-            stream.append(i)
-            continue
-        }
-        
-        /**If is true put in first*/
-        if comparator(i, stream[0]){
-            /**if stream's count is equal m remove change first one to new one*/
-            if(stream.count == m){
-                stream[0] = i
-                /*but the new item may not is the compared item so run this function keep first is for compared**/
-                letFirstIsCompared(&stream,comparator)
-            }else{
-                stream.append(i)
-            }
-        }else{
-            /*if stream's count hadn't equal m put the item anyway,but use append so it can keep first one is for compared **/
-            if stream.count < m{
-                stream.insert(i, at: 0)
-            }
+        /**if heap's count is smaller than m insert item anyway*/
+        if heap.count < m{
+            heap.insert(i, at: 0)
+            toheap(&heap, i: 0, comparator: comparator)
+        }else if comparator(i,heap[0]){
+            heap[0] = i
+            toheap(&heap, i: 0, comparator: comparator)
         }
     }
     
-    return stream
+    return heap
 }
 
-func letFirstIsCompared<T:Comparable>(_ items:inout[T],_ comparator:(T,T)->Bool){
-    for i in 1..<items.count {
-        if !comparator(items[i], items[0]) {
-            swap(&items, 0, i)
-        }
+func toheap<T:Comparable>(_ heap:inout[T],i:Int,comparator:(T,T)->Bool){
+    let l = 2*i + 1
+    let r = 2*i + 2
+    var forCompared = i
+    if l < heap.count && !comparator(heap[l], heap[forCompared]){
+        forCompared = l
+    }
+    
+    if r < heap.count && !comparator(heap[r], heap[forCompared]){
+        forCompared = r
+    }
+    
+    if forCompared != i{
+        swap(&heap, i,forCompared)
+        toheap(&heap, i: forCompared, comparator: comparator)
     }
 }
 
@@ -81,3 +78,43 @@ func swap<T:Comparable>(_ items:inout[T],_ indexa:Int, _ indexb:Int){
     items[indexa] = items[indexb]
     items[indexb] = Temp
 }
+
+
+//func findNItems<T:Comparable>(_ items:[T],_ m: Int,_ comparator:(T,T)->Bool) ->[T]{
+//    var stream:[T] = [T]()
+//    for i in items{
+//        /*if stream is empty add item anyway**/
+//        if(stream.count==0){
+//            stream.append(i)
+//            continue
+//        }
+//
+//        /**If is true put in first*/
+//        if comparator(i, stream[0]){
+//            /**if stream's count is equal m remove change first one to new one*/
+//            if(stream.count == m){
+//                stream[0] = i
+//                /*but the new item may not is the compared item so run this function keep first is for compared**/
+//                letFirstIsCompared(&stream,comparator)
+//            }else{
+//                stream.append(i)
+//            }
+//        }else{
+//            /*if stream's count hadn't equal m put the item anyway,but use append so it can keep first one is for compared **/
+//            if stream.count < m{
+//                stream.insert(i, at: 0)
+//            }
+//        }
+//    }
+//
+//    return stream
+//}
+//
+//func letFirstIsCompared<T:Comparable>(_ items:inout[T],_ comparator:(T,T)->Bool){
+//    for i in 1..<items.count {
+//        if !comparator(items[i], items[0]) {
+//            swap(&items, 0, i)
+//        }
+//    }
+//}
+
